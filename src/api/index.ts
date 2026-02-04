d2fe1cf0c5738c5a522fdaaf25d72cfd5bd216ba
Kalpan2007 <kalpankaneriya@gmail.com>
2026-02-04 10:55:03 +0530

    Add API layer for articles, blogs, reports, etc.

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Space Pulse - API Services Index
+ */
+
+export { default as apiClient } from './client';
+export { articlesApi } from './articles';
+export { blogsApi } from './blogs';
+export { reportsApi } from './reports';
+export { infoApi } from './info';
